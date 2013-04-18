adca66fbfe8995b21ccd1ab0fe2782676186d9e9
Remco Tolsma <info@remcotolsma.nl>
2013-04-18 16:35:47 +0200

    Added some spacing for easier reading.

--- includes/post.php
+++ includes/post.php
@@ -9,24 +9,23 @@ function orbis_twinfield_init() {
 add_action( 'init', 'orbis_twinfield_init' );
 
 function orbis_twinfield_customer_meta_box() {
-	
 	global $post;
-	
-	$id = get_post_meta( $post->ID, '_twinfield_customer_id', true );
-	$title = $post->post_title;
-	$post_id = $post->ID;
-	
+
+	$id         = get_post_meta( $post->ID, '_twinfield_customer_id', true );
+	$title      = $post->post_title;
+	$post_id    = $post->ID;
+
 	// Perhaps move all this into a class to represent orbis_company
 	$kvk_number = get_post_meta( $post->ID, '_orbis_company_kvk_number', true );
 	$email      = get_post_meta( $post->ID, '_orbis_company_email', true );
 	$website    = get_post_meta( $post->ID, '_orbis_company_website', true );
-	
-	$address = get_post_meta( $post->ID, '_orbis_company_address', true );
-	$postcode = get_post_meta( $post->ID, '_orbis_company_postcode', true );
-	$city = get_post_meta( $post->ID, '_orbis_company_city', true );
-	$country = get_post_meta( $post->ID, '_orbis_company_country', true );
-	
-	include ( ORBIS_TWINFIELD_FOLDER . '/views/orbis_twinfield_customer_meta_box.php' );
+
+	$address    = get_post_meta( $post->ID, '_orbis_company_address', true );
+	$postcode   = get_post_meta( $post->ID, '_orbis_company_postcode', true );
+	$city       = get_post_meta( $post->ID, '_orbis_company_city', true );
+	$country    = get_post_meta( $post->ID, '_orbis_company_country', true );
+
+	include ORBIS_TWINFIELD_FOLDER . '/views/orbis_twinfield_customer_meta_box.php';
 }
 
 add_action( 'twinfield_customer_meta_box', 'orbis_twinfield_customer_meta_box' );
