99f2615f737c1e32bf97a1cd34c7230a28d42e3e
Remco Tolsma <info@remcotolsma.nl>
2013-04-18 16:47:55 +0200

    Moved the AJAX function to the plugin class.

--- classes/orbis-twinfield-plugin.php
+++ classes/orbis-twinfield-plugin.php
@@ -7,16 +7,50 @@ class Orbis_Twinfield_Plugin extends Orbis_Plugin {
 		$this->plugin_include( 'includes/post.php' );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'wp_ajax_form_builder_submit', array( $this, 'form_builder_submit' ) );
 	}
 
 	public function loaded() {
 		$this->load_textdomain( 'orbis_twinfield', '/languages/' );
 	}
 
-	public function orbis_twinfield_assets() {
+	public function admin_enqueue_scripts() {
 		wp_register_script( 'orbis-twinfield-admin', $this->plugin_url( 'assets/orbis-twinfield-admin.js' ), array( 'jquery' ) );
 
 		wp_enqueue_script( 'orbis-twinfield-admin' );
 	}
 
+	function form_builder_submit() {
+	
+		$customer = new \Pronamic\WP\Twinfield\FormBuilder\Form\Customer();
+	
+		$data = $_POST;
+		if ( empty( $data['id'] ) ) {
+			$extra = $customer->extra_variables();
+			$data['id'] = $extra['latest_customer_id'];
+		}
+	
+		$notice = new \ZFramework\Util\Notice();
+	
+		if ( $customer->submit( $data ) ) {
+	
+			$customer_response = Pronamic\Twinfield\Customer\Mapper\CustomerMapper::map($customer->get_response());
+	
+			update_post_meta( $data['post_id'], '_twinfield_customer_id', $customer_response->getID() );
+	
+			$notice->updated( 'Successful!' );
+	
+			echo json_encode( array( 'resp' => true, 'id' => $customer_response->getID(), 'message' => $notice->retrieve() ) );
+			exit;
+		} else {
+			$errors = $customer->get_response()->getErrorMessages();
+	
+			foreach ( $errors as $error ) {
+				$notice->error( $error );
+			}
+	
+			echo json_encode( array( 'resp' => false, 'errors' => $notice->retrieve() ) );
+			exit;
+		}
+	}
 }
