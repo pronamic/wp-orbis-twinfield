e5b1070ea3bc65cee123c19f6132a58de233e4c9
Leon Rowland <leon@rowland.nl>
2013-04-18 13:17:34 +0200

    Working Form Builder Submit.  Currently uses the ZFramework/Util/Notice class.  Could require a check

--- orbis-twinfield.php
+++ orbis-twinfield.php
@@ -18,6 +18,9 @@ License: GPL
 GitHub URI: https://github.com/pronamic/wp-orbis-twinfield
 */
 
+define( 'ORBIS_TWINFIELD_FILE', __FILE__ );
+define( 'ORBIS_TWINFIELD_FOLDER', dirname( __FILE__ ) );
+
 function orbis_twinfield_bootstrap() {
 	include 'classes/orbis-twinfield-plugin.php';
 
@@ -27,3 +30,44 @@ function orbis_twinfield_bootstrap() {
 }
 
 add_action( 'orbis_bootstrap', 'orbis_twinfield_bootstrap' );
+
+add_action( 'admin_enqueue_scripts', 'orbis_twinfield_assets' );
+function orbis_twinfield_assets() {
+	wp_register_script( 'orbis-twinfield-admin', plugins_url( '/assets/orbis-twinfield-admin.js', ORBIS_TWINFIELD_FILE ), array( 'jquery' ) );
+	wp_enqueue_script( 'orbis-twinfield-admin' );
+}
+
+add_action( 'wp_ajax_form_builder_submit', 'orbis_twinfield_form_builder_submit' );
+function orbis_twinfield_form_builder_submit() {
+	
+	$customer = new \Pronamic\WP\Twinfield\FormBuilder\Form\Customer();
+	
+	$data = $_POST;
+	if ( empty( $data['id'] ) ) {
+		$extra = $customer->extra_variables();
+		//$data['id'] = $extra['latest_customer_id'];
+	}
+	
+	$notice = new \ZFramework\Util\Notice();
+	
+	if ( $customer->submit( $data ) ) {
+		
+		$customer_response = Pronamic\Twinfield\Customer\Mapper\CustomerMapper::map($customer->get_response());
+		
+		$notice->updated( 'Successful!' );
+		
+		echo json_encode( array( 'resp' => true, 'id' => $customer_response->getID(), 'message' => $notice->retrieve() ) );
+		exit;
+	} else {
+		$errors = $customer->get_response()->getErrorMessages();
+		
+		foreach ( $errors as $error ) {
+			$notice->error( $error );
+		}
+		
+		echo json_encode( array( 'resp' => false, 'errors' => $notice->retrieve() ) );
+		exit;
+	}
+	
+	
+}
---
+++ views/orbis_twinfield_customer_meta_box.php
@@ -0,0 +1,19 @@
+<div class="jFormBuilderBox">
+	<div class="jFormBuilderBoxMessages"></div>
+	<input type="hidden" name="id" value="<?php echo $id; ?>"/>
+	<input type="hidden" name="name" value="<?php echo $title; ?>"/>
+	<input type="hidden" name="website" value="<?php echo $website; ?>"/>
+	<input type="hidden" name="duedays" value="30"/>
+	<input type="hidden" name="vatcode" value="VH"/>
+	<input type="hidden" name="ebilling" value="true"/>
+	<input type="hidden" name="ebillmail" value="<?php echo $email; ?>"/>
+	<input type="hidden" name="addresses[1][default]" value="true" />
+	<input type="hidden" name="addresses[1][type]" value="invoice"/>
+	<input type="hidden" name="addresses[1][field2]" value="<?php echo $address; ?>"/>
+	<input type="hidden" name="addresses[1][field5]" value="<?php echo $kvk_number; ?>"/>
+	<input type="hidden" name="addresses[1][postcode]" value="<?php echo $postcode; ?>"/>
+	<input type="hidden" name="addresses[1][city]" value="<?php echo $city; ?>"/>
+	<input type="hidden" name="addresses[1][country]" value="<?php echo $country; ?>"/>
+	<input type="hidden" name="addresses[1][email]" value="<?php echo $email; ?>"/>
+	<input type="submit" value="<?php _e( 'Send to form builder' ); ?>"/>
+</div>
