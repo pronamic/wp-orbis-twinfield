90d9d95cee49056bc6b5fd1299500f232c8941a8
Remco Tolsma <info@remcotolsma.nl>
2013-05-08 10:49:22 +0200

    Added Twinfield invoice link.

--- includes/template.php
+++ includes/template.php
@@ -9,3 +9,12 @@ function orbis_twinfield_render_details() {
 }
 
 add_action( 'orbis_before_side_content', 'orbis_twinfield_render_details', 100 );
+
+
+function orbis_twinfield_invoice_link( $link, $invoice_number ) {
+	$link = home_url( sprintf( '/facturen/%s/', $invoice_number ) );
+
+	return $link;
+} 
+
+add_filter( 'orbis_invoice_link', 'orbis_twinfield_invoice_link', 10, 2 );
