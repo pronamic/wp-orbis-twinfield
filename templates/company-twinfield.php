7a2509eb51df25940bcdb40406cfd9ebaca05adf
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-02-27 12:06:01 +0100

    Converted Bootstrap panels to cards.

--- templates/company-twinfield.php
+++ templates/company-twinfield.php
@@ -6,29 +6,30 @@ $twinfield_customer_id = get_post_meta( $post->ID, '_twinfield_customer_id', tru
 
 if ( ! empty( $twinfield_customer_id ) ) : ?>
 	
-	<div class="panel">
-		<header>
-			<h3><?php esc_html_e( 'Twinfield', 'orbis_twinfield' ); ?></h3>
-		</header>
-	
-		<div class="content">
-			<dl>
-				<dt><?php esc_html_e( 'Twinfield ID', 'orbis_twinfield' ); ?></dt>
-				<dd>
-					<?php
+	<div class="card">
+		<div class="card-body">
+			<h3 class="card-title"><?php esc_html_e( 'Twinfield', 'orbis_twinfield' ); ?></h3>
+		
+			<div class="content">
+				<dl>
+					<dt><?php esc_html_e( 'Twinfield ID', 'orbis_twinfield' ); ?></dt>
+					<dd>
+						<?php
 
-					$url = home_url( sprintf( '/debiteuren/%s/', $twinfield_customer_id ) );
+						$url = home_url( sprintf( '/debiteuren/%s/', $twinfield_customer_id ) );
 
-					printf(
-						'<a href="%s" target="_blank">%s</a>',
-						esc_attr( $url ),
-						esc_html( $twinfield_customer_id )
-					);
+						printf(
+							'<a href="%s" target="_blank">%s</a>',
+							esc_attr( $url ),
+							esc_html( $twinfield_customer_id )
+						);
 
-					?>
-				</dd>
-			</dl>
+						?>
+					</dd>
+				</dl>
+			</div>
 		</div>
+		
 	</div>
 
 <?php endif; ?>
