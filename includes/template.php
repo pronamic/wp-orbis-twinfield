d22c0e15894b1a96b3e271711d89349e6f0a9735
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-03-23 13:31:12 +0100

    Removed template that was no longer used.

--- includes/template.php
+++ includes/template.php
@@ -1,16 +1,5 @@
 <?php
 
-function orbis_twinfield_render_details() {
-	if ( is_singular( 'orbis_company' ) ) {
-		global $orbis_twinfield_plugin;
-
-		$orbis_twinfield_plugin->plugin_include( 'templates/company-twinfield.php' );
-	}
-}
-
-add_action( 'orbis_before_side_content', 'orbis_twinfield_render_details', 100 );
-
-
 function orbis_twinfield_invoice_link( $link, $invoice_number ) {
 	if ( ! empty( $invoice_number ) ) {
 		$link = home_url( sprintf( '/facturen/%s/', $invoice_number ) );
